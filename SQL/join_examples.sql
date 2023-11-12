bd4f739f55a08354294bc7a41c2e85fdbdf77e90
Oleg <oleg-dmitriewich@yandex.ru>
2023-11-12 21:15:08 +0200

    Simple examples of JOIN

---
+++ SQL/join_examples.sql
@@ -0,0 +1,22 @@
+SELECT product_name, company_name, units_in_stock
+FROM products
+INNER JOIN suppliers
+ON products.supplier_id = suppliers.supplier_id
+ORDER BY units_in_stock DESC;
+
+SELECT SUM(units_in_stock), category_name
+FROM products
+JOIN categories
+ON products.category_id = categories.category_id
+GROUP BY category_name
+ORDER BY SUM(units_in_stock) DESC
+LIMIT 5;
+
+SELECT SUM(unit_price*units_in_stock), category_name
+FROM products
+JOIN categories
+ON products.category_id = categories.category_id
+WHERE discontinued !=1
+GROUP BY category_name
+HAVING SUM(unit_price*units_in_stock) > 10000
+ORDER BY SUM(unit_price*units_in_stock) DESC;
