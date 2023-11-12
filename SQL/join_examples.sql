d1bea3f552be775c1f0790fec43e6e1d10c64ea9
Oleg <oleg-dmitriewich@yandex.ru>
2023-11-12 23:10:09 +0200

    usage contraction

--- SQL/join_examples.sql
+++ SQL/join_examples.sql
@@ -7,7 +7,8 @@ ORDER BY units_in_stock DESC;
 SELECT SUM(units_in_stock), category_name
 FROM products
 JOIN categories
-ON products.category_id = categories.category_id
+--ON products.category_id = categories.category_id
+USING (category_id)
 GROUP BY category_name
 ORDER BY SUM(units_in_stock) DESC
 LIMIT 5;
@@ -15,11 +16,23 @@ LIMIT 5;
 SELECT SUM(unit_price*units_in_stock), category_name
 FROM products
 JOIN categories
-ON products.category_id = categories.category_id
+--ON products.category_id = categories.category_id
+USING (category_id)
 WHERE discontinued !=1
 GROUP BY category_name
 HAVING SUM(unit_price*units_in_stock) > 10000
 ORDER BY SUM(unit_price*units_in_stock) DESC;
+
+
+-- An example of usage contraction:
+SELECT category_id, SUM(unit_price * units_in_stock) AS total_price
+FROM products
+WHERE discontinued != 1
+GROUP BY category_id
+HAVING SUM(unit_price * units_in_stock) > 5000
+ORDER BY total_price;
+
+
 
 
 
